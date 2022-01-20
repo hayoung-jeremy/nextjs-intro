ad6b94d22fc8e509ae3aae03fda9132f3f2c5b6b
Ha young, Kim <jeremiah91@naver.com>
2022-01-20 13:42:23 +0900

    feat : Server Side Rendering  - getServerSideProps()

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const Home = () => {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+const Home = ({ results }) => {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -45,3 +37,16 @@ const Home = () => {
 };
 
 export default Home;
+
+// getServerSideProps : 이 안의 내용은 반드시 서버에서만 실행됨
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    // props 는 Home component 에게 props 로 전달됨
+    props: {
+      results,
+    },
+  };
+}
